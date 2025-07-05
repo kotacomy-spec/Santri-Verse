40355d5b403188807c4faaf05ff03c154eb20d62
navanken <afandinaufal67@gmail.com>
2025-07-05 19:28:11 +0700

    Fitur Login

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -8,7 +8,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Toaster, toast } from "sonner";
+import { toast } from "sonner";
 import { Mail, Lock, Eye, EyeOff } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
