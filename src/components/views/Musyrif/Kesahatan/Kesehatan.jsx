9828214078d1b58bcaa6b1778412c8bcb019957b
navanken <afandinaufal67@gmail.com>
2025-07-08 22:53:29 +0700

    Testing Commit

---
+++ src/components/views/Musyrif/Kesahatan/Kesehatan.jsx
@@ -0,0 +1,397 @@
+import React, { useEffect, useState } from "react";
+import {
+  Search,
+  Filter,
+  Eye,
+  MoreHorizontal,
+  UserCircle,
+  ChevronDown,
+  ChevronLeft,
+  ChevronRight,
+} from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Badge } from "@/components/ui/badge";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Card } from "@/components/ui/card";
+import { supabase } from "@/lib/supabase/supabaseClient";
+import CreateKesehatan from "./CreateDialog/KesehatanCreate";
+import DeleteKesehatanRecordDialog from "./DeleteDialog/Delete";
+import { Link } from "react-router-dom";
+
+const Kesehatan = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [statusFilter, setStatusFilter] = useState("all");
+  const [priorityFilter, setPriorityFilter] = useState("all");
+  const [kesehatanSantri, setKesehatanSantri] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [createDialog, setCreateDialog] = useState(false);
+  const [deleteId, setDeleteId] = useState(null);
+  const [filteredData, setFilteredData] = useState("all");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const getKesehatanSantri = async () => {
+    setLoading(true);
+    const { data: kesehatanData } = await supabase
+      .from("kesehatan_santri")
+      .select(`*, santri:santri_id(id, nama)`)
+      .order("created_at", { ascending: false });
+
+    setKesehatanSantri(kesehatanData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getKesehatanSantri();
+
+    const channel = supabase
+      .channel("kesehatan_santri_changes")
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          schema: "public",
+          table: "kesehatan_santri",
+        },
+        async (payload) => {
+          if (payload.eventType === "INSERT") {
+            const { data: NewRecord } = await supabase
+              .from("kesehatan_santri")
+              .select(`*, santri:santri_id(id, nama)`)
+              .eq("id", payload.new.id)
+              .single();
+
+            if (NewRecord) {
+              // setKesehatanSantri((prev) => [...prev, NewRecord]);
+              setKesehatanSantri((prev) => [NewRecord, ...prev]);
+            }
+          } else if (payload.eventType === "UPDATE") {
+            const { data: updatedRecord } = await supabase
+              .from("kesehatan_santri")
+              .select(`*, santri:santri_id(id, nama)`)
+              .eq("id", payload.new.id)
+              .single();
+            if (updatedRecord) {
+              // setKesehatanSantri((prev) =>
+              //   prev.map((item) =>
+              //     item.id === payload.new.id ? updatedRecord : item
+              //   )
+              // );
+              setKesehatanSantri((prev) => {
+                const filtered = prev.filter(
+                  (item) => item.id !== payload.new.id
+                );
+                return [updatedRecord, ...filtered];
+              });
+            }
+          } else if (payload.eventType === "DELETE") {
+            setKesehatanSantri((prev) =>
+              prev.filter((item) => item.id !== payload.old.id)
+            );
+          }
+        }
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, []);
+
+  useEffect(() => {
+    const filtered = kesehatanSantri.filter((item) => {
+      const statusMatch =
+        statusFilter === "all" || item.status === statusFilter;
+      const priorityMatch =
+        priorityFilter === "all" || item.prioritas === priorityFilter;
+
+      const searchMatch = item.santri.nama
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase());
+
+      return searchMatch && statusMatch && priorityMatch;
+    });
+    setFilteredData(filtered);
+  }, [searchTerm, statusFilter, priorityFilter, kesehatanSantri]);
+
+  const totalPages = Math.ceil(filteredData.length / rowsPerPage);
+  const paginatedData = filteredData.slice(
+    (currentPage - 1) * rowsPerPage,
+    currentPage * rowsPerPage
+  );
+
+  const getStatusVariant = (status) => {
+    switch (status) {
+      case "Dalam Perawatan":
+        return "bg-sky-500";
+      case "Selesai":
+        return "bg-green-700";
+      case "Menunggu":
+        return "bg-gray-200 text-black";
+      default:
+        return "bg-gray-200 text-black";
+    }
+  };
+
+  const getPriorityVariant = (priority) => {
+    switch (priority) {
+      case "Tinggi":
+        return "bg-red-500";
+      case "Sedang":
+        return "bg-orange-500";
+      case "Rendah":
+        return "bg-gray-200 text-black";
+      default:
+        return "bg-gray-200 text-black";
+    }
+  };
+
+  return (
+    <>
+      <CreateKesehatan
+        dialogstate={createDialog}
+        setDialogState={setCreateDialog}
+      />
+      <DeleteKesehatanRecordDialog
+        deleteId={deleteId}
+        setDialogState={setDeleteId}
+      />
+      <div className="bg-background text-foreground">
+        <Card>
+          <div className="container mx-auto p-6">
+            <div className="flex justify-between items-center mb-6">
+              <div>
+                <h1 className="text-3xl font-bold tracking-tight">
+                  Data Kesehatan Santri
+                </h1>
+                <p className="text-muted-foreground mt-1">
+                  Kelola data kesehatan santri.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex flex-wrap gap-4 mb-6">
+              <div className="relative flex-1 min-w-64">
+                <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-muted-foreground w-4 h-4" />
+                <Input
+                  placeholder="Cari santri..."
+                  className="pl-10"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
+              </div>
+
+              <div className="flex gap-2">
+                <Select value={statusFilter} onValueChange={setStatusFilter}>
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Status" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="all">Semua Status</SelectItem>
+                    <SelectItem value="Dalam Perawatan">
+                      Dalam Perawatan
+                    </SelectItem>
+                    <SelectItem value="Selesai">Selesai</SelectItem>
+                    <SelectItem value="Menunggu">Menunggu</SelectItem>
+                  </SelectContent>
+                </Select>
+
+                <Select
+                  value={priorityFilter}
+                  onValueChange={setPriorityFilter}
+                >
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Prioritas" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="all">Semua Prioritas</SelectItem>
+                    <SelectItem value="Tinggi">Tinggi</SelectItem>
+                    <SelectItem value="Sedang">Sedang</SelectItem>
+                    <SelectItem value="Rendah">Rendah</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+
+              <div className="flex gap-2">
+                <Button
+                  className={`bg-green-700 hover:bg-green-800 cursor-pointer`}
+                  onClick={() => setCreateDialog(true)}
+                >
+                  Tambah Data
+                </Button>
+              </div>
+            </div>
+
+            <div className="rounded-md border">
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>No Refrensi</TableHead>
+                    <TableHead>Nama Santri</TableHead>
+                    <TableHead>Keluhan</TableHead>
+                    <TableHead>Prioritas</TableHead>
+                    <TableHead>Status</TableHead>
+                    <TableHead>Tanggal</TableHead>
+                    <TableHead className="w-[50px]"></TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {isLoading ? (
+                    [...Array(5)].map((_, index) => (
+                      <TableRow key={index}>
+                        <TableCell>
+                          <div className="h-4 bg-gray-200 rounded animate-pulse"></div>
+                        </TableCell>
+                        <TableCell>
+                          <div className="h-4 bg-gray-200 rounded animate-pulse"></div>
+                        </TableCell>
+                        <TableCell>
+                          <div className="h-4 bg-gray-200 rounded animate-pulse w-3/4"></div>
+                        </TableCell>
+                        <TableCell>
+                          <div className="h-6 bg-gray-200 rounded-full animate-pulse w-20"></div>
+                        </TableCell>
+                        <TableCell>
+                          <div className="h-6 bg-gray-200 rounded-full animate-pulse w-16"></div>
+                        </TableCell>
+                        <TableCell>
+                          <div className="h-4 bg-gray-200 rounded animate-pulse w-24"></div>
+                        </TableCell>
+                        <TableCell>
+                          <div className="h-8 w-8 bg-gray-200 rounded animate-pulse"></div>
+                        </TableCell>
+                      </TableRow>
+                    ))
+                  ) : paginatedData.length === 0 ? (
+                    <TableRow>
+                      <TableCell
+                        colSpan={8}
+                        className="text-center text-gray-500"
+                      >
+                        Data tidak ditemukan.
+                      </TableCell>
+                    </TableRow>
+                  ) : (
+                    paginatedData.map((item) => (
+                      <TableRow key={item.id}>
+                        <TableCell className="font-medium">
+                          KS-{item.id}
+                        </TableCell>
+                        <TableCell>{item.santri.nama}</TableCell>
+                        <TableCell>{item.keluhan}</TableCell>
+                        <TableCell className="max-w-[300px]">
+                          <Badge className={getPriorityVariant(item.prioritas)}>
+                            <div className=" truncate">{item.prioritas}</div>
+                          </Badge>
+                        </TableCell>
+                        <TableCell>
+                          <Badge className={getStatusVariant(item.status)}>
+                            {item.status}
+                          </Badge>
+                        </TableCell>
+                        <TableCell>{item.tgl_diperiksa}</TableCell>
+                        <TableCell>
+                          <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                              <Button variant="ghost" className="h-8 w-8 p-0">
+                                <MoreHorizontal className="h-4 w-4" />
+                              </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                              <DropdownMenuItem asChild>
+                                <Link
+                                  to={`/musyrif/kesehatan/detail/${item.id}`}
+                                >
+                                  Detail
+                                </Link>
+                              </DropdownMenuItem>
+                              <DropdownMenuItem
+                                onClick={() => setDeleteId(item.id)}
+                                className="text-red-600 "
+                              >
+                                Hapus
+                              </DropdownMenuItem>
+                            </DropdownMenuContent>
+                          </DropdownMenu>
+                        </TableCell>
+                      </TableRow>
+                    ))
+                  )}
+                </TableBody>
+              </Table>
+            </div>
+            <div className="flex items-center justify-between mt-6">
+              <div className="flex items-center gap-2">
+                <span className="text-sm text-muted-foreground">
+                  Baris per halaman
+                </span>
+                <Select
+                  value={String(rowsPerPage)}
+                  onValueChange={(value) => {
+                    setRowsPerPage(Number(value));
+                    setCurrentPage(1);
+                  }}
+                >
+                  <SelectTrigger className="w-[70px]">
+                    <SelectValue />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="10">10</SelectItem>
+                    <SelectItem value="20">20</SelectItem>
+                    <SelectItem value="50">50</SelectItem>
+                  </SelectContent>
+                </Select>
+                <span className="text-sm text-muted-foreground mx-4">
+                  Halaman {currentPage} dari {totalPages || 1}
+                </span>
+                <div className="flex gap-1">
+                  <Button
+                    variant="outline"
+                    size="sm"
+                    disabled={currentPage === 1}
+                    onClick={() => setCurrentPage((prev) => prev - 1)}
+                  >
+                    <ChevronLeft className="h-4 w-4" />
+                  </Button>
+
+                  <Button
+                    variant="outline"
+                    size="sm"
+                    disabled={currentPage === totalPages || totalPages === 0}
+                    onClick={() => setCurrentPage((prev) => prev + 1)}
+                  >
+                    <ChevronRight className="h-4 w-4" />
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default Kesehatan;
